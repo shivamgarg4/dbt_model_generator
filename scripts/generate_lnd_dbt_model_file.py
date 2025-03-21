dd583ead2d96f8c27d47ca6b3513ce2885c5b1d0
shivamgarg4 <shivam.garg@cbrands.com>
2025-03-21 11:09:30 +0530

    DP View And Sample Mapping Functionality Addtion

--- scripts/generate_lnd_dbt_model_file.py
+++ scripts/generate_lnd_dbt_model_file.py
@@ -166,3 +166,118 @@ def get_snowflake_connection(config):
         return conn
     except Exception as e:
         raise Exception(f"Failed to connect to Snowflake: {str(e)}")
+
+def create_dp_view_file(config, file_path):
+    """Generate the dbt model file."""
+    try:
+        with open(config) as f:
+            config = json.load(f)
+
+        # Extract source information
+        source_name = config['Source']['Name']
+
+        workbook = load_workbook(file_path)
+        mapping_sheet, config_sheet = workbook['Mapping'], workbook['Config']
+
+        """Extract target and source table information from mapping sheet"""
+        target_table = None
+        source_table = None
+
+        for row in range(1, mapping_sheet.max_row + 1):
+            cell_value = mapping_sheet.cell(row=row, column=1).value
+            if cell_value == 'TARGET_TABLE':
+                target_table = mapping_sheet.cell(row=row, column=2).value
+            elif cell_value == 'SOURCE_TABLE':
+                source_table = mapping_sheet.cell(row=row, column=2).value
+            if target_table and source_table:
+                break
+
+        if not source_table:
+            raise ValueError("Source table not found in mapping sheet")
+
+        source_db, source_schema, source_table_name = source_table.split('.')
+        target_parts = target_table.split('.')
+        target_schema = target_parts[0] if len(target_parts) > 1 else None
+        target_table_name = target_parts[-1]
+
+        """Extract Snowflake configuration from Config sheet"""
+        snowflake_config = {}
+        in_snowflake_section = False
+
+        for row in range(2, config_sheet.max_row + 1):
+            param = config_sheet.cell(row=row, column=1).value
+            if not param:
+                continue
+            if param == 'Snowflake Configuration':
+                in_snowflake_section = True
+                continue
+            if in_snowflake_section and param in ['ROLE', 'WAREHOUSE', 'DATABASE', 'ACCOUNT', 'USER', 'AUTHENTICATOR']:
+                value = config_sheet.cell(row=row, column=2).value
+                if value:
+                    snowflake_config[param] = value
+
+        """Get column information from Snowflake"""
+        conn = None
+        cursor = None
+        conn = snowflake.connector.connect(
+            account=snowflake_config['ACCOUNT'],
+            user=snowflake_config['USER'],
+            authenticator=snowflake_config['AUTHENTICATOR'],
+            warehouse=snowflake_config['WAREHOUSE'],
+            role=snowflake_config['ROLE'],
+            database=source_db,
+            schema=source_schema
+        )
+
+        print(conn)
+
+        cursor = conn.cursor()
+        cursor.execute(f"""
+               SELECT COLUMN_NAME
+               FROM {source_db}.INFORMATION_SCHEMA.COLUMNS
+                WHERE TABLE_SCHEMA = '{source_schema}'
+               AND TABLE_NAME = '{source_table_name}'
+               ORDER BY ORDINAL_POSITION
+           """)
+
+        columns = cursor.fetchall()
+        cursor.execute(f"""
+               SELECT COLUMN_NAME
+               FROM {source_db}.INFORMATION_SCHEMA.COLUMNS
+                WHERE TABLE_SCHEMA = '{target_schema}'
+               AND TABLE_NAME = '{target_table_name}'
+               ORDER BY ORDINAL_POSITION
+           """)
+        target_columns = cursor.fetchall()
+        source_formatted_columns = format_columns(columns)
+        source_replaced_columns = replace_audit_columns(source_formatted_columns, source_schema, source_table_name, target_schema,
+                                                 target_table_name)
+        target_formatted_columns = format_columns(target_columns)
+        model_config = f"""
+           -- Model: {target_schema}.{target_table_name}
+           -- Source: {source_db}.{source_schema}.{source_table_name}
+           """
+
+        model_config += f"\n\ncreate or replace view {target_schema}.{target_table_name}(\n"
+        model_config += ",\n".join(target_formatted_columns) + "\n)copy grants as\n"
+        model_config += "\n\nSELECT\n"
+        model_config += ",\n".join(source_replaced_columns) + "\n"
+        model_config += f"FROM {source_schema}.{source_table_name}\n"
+
+        # Create output directory if it doesn't exist
+        output_dir = 'views'
+        os.makedirs(output_dir, exist_ok=True)
+
+        # Generate file name
+        model_name = f"{target_schema}.{target_table_name}"
+        file_name = f"{model_name}.sql"
+        file_path = os.path.join(output_dir, file_name)
+
+        # Write model file
+        with open(file_path, 'w') as f:
+            f.write(model_config)
+
+        return True, file_path
+    except Exception as e:
+        raise Exception(f"An error occurred while generating the dbt model: {e}")
+
